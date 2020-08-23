039bb5bd8772e6e4605c163fb7bf1289dd3a5878
Barbar980 <48921668+Barbar980@users.noreply.github.com>
2020-08-23 20:47:06 +0200

    Update run.py

--- run.py
+++ run.py
@@ -1,28 +1,11 @@
-# we use the right library
-
 import requests
-
 city = input("Enter Your city: ")
-
-# downloading page by get
-
-
 res = requests.get('http://api.openweathermap.org/data/2.5/weather?q={}&appid=dbed2c52ca15d6395ec6241e25d4a536&units=metric'.format(city))
 data = res.json()
-
-# choosing weather parameters from data
-# first sqare bracker show category, second value
-
-
 temp = data["main"]["temp"]
 wind_speed = data["wind"]["speed"]
 description = data["weather"][0]["description"]
 
-
-# Puting data to weather desciption by format function
-
-
 print("Temperature: ", temp)
 print("Wind speed: {}".format(wind_speed))
 print("Description: {}".format(description))
-
